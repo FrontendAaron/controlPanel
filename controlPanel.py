b3f94735c50bf57a60908bb93cfebcd5daffd476
Aaron Shippey <aaronshippey.bus@gmail.com>
2025-03-22 17:44:42 -0400

    Create controlPanel.py

---
+++ controlPanel.py
@@ -0,0 +1,12 @@
+frequency = ""
+print("1. Ultrasonic Testing(1-100 MHz)")
+print("2. Eddy Current Testing (10-10,000 kHz)")
+print("3. X-ray Testing (100-450 kV)")
+input("Enter the number for the chose method: 2")
+
+method = input("Enter the number for the chosen method: ")
+
+if method == 1:
+    input("Enter a frequency (1-100 MHz for Ultrasonic) ")
+elif method == 2:
+    input("Enter a frequency (1-100 MHz for Ultrasonic) ")
