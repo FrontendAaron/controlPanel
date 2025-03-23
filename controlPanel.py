c9ac60212c65a0f524216b4c7a1f1eb3177de378
Aaron Shippey <aaronshippey.bus@gmail.com>
2025-03-23 09:44:16 -0400

    add random and time modules

--- controlPanel.py
+++ controlPanel.py
@@ -1,12 +1,20 @@
-frequency = ""
+
+
 print("1. Ultrasonic Testing(1-100 MHz)")
 print("2. Eddy Current Testing (10-10,000 kHz)")
 print("3. X-ray Testing (100-450 kV)")
-input("Enter the number for the chose method: 2")
 
-method = input("Enter the number for the chosen method: ")
+method = int(input("Enter the number for the "
+"chosen method: \nPRESS ANY OTHER KEY TO EXIT\n"))
 
-if method == 1:
-    input("Enter a frequency (1-100 MHz for Ultrasonic) ")
-elif method == 2:
-    input("Enter a frequency (1-100 MHz for Ultrasonic) ")
+while True:
+
+        
+        if method == 1:
+            frequncy = int(input("Enter a frequency (1-100 MHz for Ultrasonic) "))
+        elif method == 2:
+            frequency = int(input("Enter a frequency (10-10,000 kHz for Eddy Current) "))
+        elif method == 3:
+            frequncy = int(input("100-450 kV for X-ray "))
+        else:
+             break
